406f006afca1ecc2144bfef8c8f7ea7cd1baa2fa
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-02-22 22:13:34 -0600

    feat: add main routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import GlobalStyle from './styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme'
 
 import App from './App'
+import Home from './pages/Home'
+import Translation from './pages/Translation/Index'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<Home />} />
+            <Route path='translation/:word' element={<Translation />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
