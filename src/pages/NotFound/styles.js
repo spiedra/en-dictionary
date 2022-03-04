112d69bc773e5ae4d84e03abff7d10e90c390e04
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-03-03 18:28:47 -0600

    feat: add goBack link to notFound page

---
+++ src/pages/NotFound/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const NotFoundContainer = styled.section`
+  ${(props) => props.theme.mixins.flexCenter}
+  flex-direction: column;
+  font-size: 1.1em;
+  padding: 8em 0;
+  text-align: center;
+  width: 90%;
+
+  a {
+    text-decoration: underline;
+
+    &:hover {
+      color: var(--primary-color);
+    }
+  }
+`
