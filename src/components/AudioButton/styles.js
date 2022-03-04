1d80506e07af532ebea3de063391f31f8e2c791e
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-03-04 00:52:46 -0600

    fix: delete audio button box-shadow

--- src/components/AudioButton/styles.js
+++ src/components/AudioButton/styles.js
@@ -9,7 +9,6 @@ export const WordAudioBtn = styled.button`
   svg {
     background-color: var(--neutral-color);
     border-radius: 50%;
-    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
     color: var(--title-color);
     cursor: pointer;
     font-size: 1.8em;
@@ -18,7 +17,11 @@ export const WordAudioBtn = styled.button`
     transition: var(--main-transition);
 
     &:hover {
-      transform: translateY(-3px);
+      transform: scale(1.1);
+    }
+
+    &:active {
+      transform: scale(0.95);
     }
   }
 `
