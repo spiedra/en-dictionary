755c54a67a01adea4d1b2b170754c00ed9657d3e
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-03-03 22:16:51 -0600

    style: update footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,11 +2,9 @@ import styled from 'styled-components'
 
 export const FooterContainer = styled.footer`
   ${(props) => props.theme.mixins.flexCenter}
-  background-color: var(--title-color);
   height: 3em;
 
   span {
-    color: var(--background-color);
     font-size: 13px;
   }
 `
